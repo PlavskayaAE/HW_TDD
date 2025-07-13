c400e0254b0abe3a6ac1ccd72dca60601561e831
Aleksandra Plavskaya <a.ichetkina@gmail.com>
2025-07-13 19:48:42 +0300

    printAllNames implemented

--- src/main/java/ru/netology/PhoneBook.java
+++ src/main/java/ru/netology/PhoneBook.java
@@ -2,14 +2,16 @@ package ru.netology;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeSet;
 
 public class PhoneBook {
     private static Map<String, String> contactsKeyIsName = new HashMap<>();
     private static Map<String, String> contactsKeyIsNumber = new HashMap<>();
+    private static TreeSet<String> names = new TreeSet<>();
 
     public static int add(String name, String number) {
         if (name == null || number == null || name.isEmpty() || number.isEmpty()) {
-            System.out.println("Oшибка! Имя контакта и номер должны быть заполнены!");
+            System.out.println("Ошибка! Имя контакта и номер должны быть заполнены!");
         } else {
             if (number.length() != 11) {
                 System.out.println("Ошибка! Номер должен состоять из 11 символов!");
@@ -37,6 +39,7 @@ public class PhoneBook {
 
 
     public static String printAllNames() {
-        return null;
+        names.addAll(contactsKeyIsName.keySet());
+        return names.toString();
     }
 }
